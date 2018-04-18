93f4add3d44b53299e726293a63a51b180745a49
olwenwilson <32312981+olwenwilson@users.noreply.github.com>
2018-04-18 18:57:45 +0100

    edited

--- mailconfirmation.py
+++ mailconfirmation.py
@@ -9,16 +9,12 @@ def send_confirmation_message(name, email):
         data={"from": "Olwen Wilson <olwenwilson95@gmail.com>",
               "to": [email],
               "subject": [name],
-              "text": "True."})
+              "text": "Thanks for submitting your TV show idea! We will read through your pitch and get back to you within 3 weeks to discuss next steps."})
 
-app = Flask("MyApp")
+app = Flask("confirmReceipt")
 
-@app.route("/registration")
-def email_registration():
-	return render_template("emails_registration.html")
-
-@app.route("/signup", methods=['POST'])
-def sign_up():
+@app.route("/emails_registration", methods=['GET','POST'])
+def emails_registration():
 	form_data = request.form
 	name = form_data["name"]
 	email = form_data["email"]
@@ -26,7 +22,6 @@ def sign_up():
 	genre = form_data["genre"]
 	topline = form_data["topline"]
 	summary = form_data["summary"]
-	}
 
 	print name
 	print email
@@ -37,7 +32,47 @@ def sign_up():
 
 	send_simple_message("Hello {}".format(name), email)
 
-	return "ALL OK"
-
 app.run(debug=True)
+
+
+#@app.route("/emails_registration", methods=['POST'])
+#def sign_up():
+#	form_data = request.form
+#	name = form_data["name"]
+#	email = form_data["email"]
+#	title = form_data["title"]
+#	genre = form_data["genre"]
+#	topline = form_data["topline"]
+#	summary = form_data["summary"]
+
+#	print name
+#	print email
+#	print title
+#	print genre
+#	print topline
+#	print summary
+
+
+
+#def send_confirmation_message():
+#	form_data = request.form
+#	name = form_data["name"]
+#	email = form_data["email"]
+#	title = form_data["title"]
+#	genre = form_data["genre"]
+#	topline = form_data["topline"]
+#	summary = form_data["summary"]
+#   return requests.post(
+#      "https://api.mailgun.net/v3/sandbox37d8b5fe16294488948620c354c7eabb.mailgun.org/messages",
+#      auth=("api", "key-d69efd003013847104f007cf8d871a3d"),
+#      data={"from": "SHOW.me Team <olwenwilson95@gmail.com>",
+#             "to": email
+#             "subject": "Thanks for submitting your idea to SHOW.me!",
+#             "text": "Thanks for submitting your TV show idea! We will read through your pitch and get back to you within 3 weeks to discuss next steps.",
+#             "html": 	<html>
+#            				<body>
+#								<p>"Thanks for submitting your TV show idea!"</p>
+#								<p>"We will read through your pitch and get back to you within 3 weeks to discuss next steps."</p>
+#							</body> 
+#						</html>
 
